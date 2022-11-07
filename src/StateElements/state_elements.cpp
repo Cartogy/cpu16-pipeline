e5faa1ad0f0c4268e7f089a9127b9fbc3d439269
Jonas Mun <cryptic_outcast@hotmail.com>
2022-11-07 20:26:32 +0000

    Tested Setting of current instruction

--- src/StateElements/state_elements.cpp
+++ src/StateElements/state_elements.cpp
@@ -3,3 +3,7 @@
 void StateElements::set_instruction(uint16_t ins) {
     current_instruction = ins;
 }
+
+uint16_t StateElements::get_current_instruction() {
+    return current_instruction;
+}
--- tests/state_elements_test.cc
+++ tests/state_elements_test.cc
@@ -1,1 +1,15 @@
 #include <gtest/gtest.h>
+#include "StateElements/state_elements.hpp"
+
+class StateElementsTest : public ::testing::Test {
+    protected:
+        StateElements se;
+};
+
+TEST_F(StateElementsTest, InstructionTest) {
+    se.set_instruction(2);
+    EXPECT_EQ(2, se.get_current_instruction());
+    se.set_instruction(6);
+    EXPECT_EQ(6, se.get_current_instruction());
+}
+
